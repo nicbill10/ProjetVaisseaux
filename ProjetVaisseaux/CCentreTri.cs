e7fb0084bfe9f2f9b4b278744d0d22104e484f19
Charles Bouchard-Tremblay <charlesbt_9@hotmail.com>
2016-11-21 13:51:31 -0500

    push lundi 21 nov apres-midi

--- ProjetVaisseaux/CCentreTri.cs
+++ ProjetVaisseaux/CCentreTri.cs
@@ -8,6 +8,11 @@ namespace ProjetVaisseaux
 {
     class CCentreTri
     {
+        Stack<CPapier> pilePapier = new Stack<CPapier>();
+        Stack<CVerre> pileVerre = new Stack<CVerre>();
+        Stack<CPlastique> pilePlastique = new Stack<CPlastique>();
+        Stack<CFerraille> pileFerraille = new Stack<CFerraille>();
+        Stack<CTerreContaminee> pileTerre = new Stack<CTerreContaminee>();
         public CCentreTri(string type)
         {
 
--- ProjetVaisseaux/CDeroulement.cs
+++ ProjetVaisseaux/CDeroulement.cs
@@ -29,6 +29,22 @@ namespace ProjetVaisseaux
                     fileVaisseaux.Enqueue(new CVaisseauCargo());
                 }
             }
+
+            int cpt = 0;
+            foreach(CVaisseau vaisseau in fileVaisseaux)
+            {
+                cpt++;
+                int quantitePapier = random.Next(1, (vaisseau.CAPACITEMAX - 3));
+                int quantiteVerre = random.Next(1, vaisseau.CAPACITEMAX - quantitePapier - 2);
+                int quantitePlastique = random.Next(1, vaisseau.CAPACITEMAX - quantitePapier - quantiteVerre - 1);
+                int quantiteFerraille = random.Next(1, vaisseau.CAPACITEMAX - quantitePapier - quantiteVerre - quantitePlastique);
+                int quantiteTerre = vaisseau.CAPACITEMAX - quantitePapier - quantiteVerre - quantitePlastique - quantiteFerraille;
+
+                int sommeMateriaux = quantitePapier + quantiteVerre + quantitePlastique + quantiteFerraille + quantiteTerre;
+
+                Console.WriteLine(cpt + " " + vaisseau.CAPACITEMAX + " " + quantitePapier + " " + quantiteVerre + " " + quantitePlastique + " " + quantiteFerraille + " " + quantiteTerre + " " + sommeMateriaux);
+            }
+                        
             Console.ReadKey();
         }
     }
